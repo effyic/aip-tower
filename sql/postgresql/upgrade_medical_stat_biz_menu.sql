24d2e65a9765ed6a155a4a6ba5ab7da98199d365
leihui <leihui1123@gmail.com>
2026-08-03 11:19:12 +0800

    feat:去掉按钮菜单

--- sql/postgresql/upgrade_medical_stat_biz_menu.sql
+++ sql/postgresql/upgrade_medical_stat_biz_menu.sql
@@ -11,14 +11,6 @@ INSERT INTO system_biz_menu (
 (6907, '用户数据统计', 'medical:stat:user', '', 2, 1, 6906, 'user', '', 'DataMonitor/user/index', 'DataMonitorUser',
  0, true, true, true, '1', CURRENT_TIMESTAMP, '1', CURRENT_TIMESTAMP, 0),
 (6908, '运营数据监控', 'medical:stat:ops', '', 2, 2, 6906, 'ops', '', 'DataMonitor/ops/index', 'DataMonitorOps',
- 0, true, true, true, '1', CURRENT_TIMESTAMP, '1', CURRENT_TIMESTAMP, 0),
-(6909, '监控数据查询', 'medical:stat:user:query', 'medical:stat:query', 3, 1, 6907, '', '', '', NULL,
- 0, true, true, true, '1', CURRENT_TIMESTAMP, '1', CURRENT_TIMESTAMP, 0),
-(6910, '监控数据查询', 'medical:stat:ops:query', 'medical:stat:query', 3, 1, 6908, '', '', '', NULL,
- 0, true, true, true, '1', CURRENT_TIMESTAMP, '1', CURRENT_TIMESTAMP, 0),
-(6911, '监控配置查询', 'medical:stat:ops:config:query', 'medical:stat:config:query', 3, 2, 6908, '', '', '', NULL,
- 0, true, true, true, '1', CURRENT_TIMESTAMP, '1', CURRENT_TIMESTAMP, 0),
-(6912, '监控配置更新', 'medical:stat:ops:config:update', 'medical:stat:config:update', 3, 3, 6908, '', '', '', NULL,
  0, true, true, true, '1', CURRENT_TIMESTAMP, '1', CURRENT_TIMESTAMP, 0)
 ON CONFLICT (id) DO UPDATE SET
     name = EXCLUDED.name,
@@ -39,4 +31,4 @@ ON CONFLICT (id) DO UPDATE SET
     updater = '1',
     update_time = CURRENT_TIMESTAMP;
 
-SELECT setval('system_biz_menu_seq', GREATEST((SELECT COALESCE(MAX(id), 1) FROM system_biz_menu), 6912));
+SELECT setval('system_biz_menu_seq', GREATEST((SELECT COALESCE(MAX(id), 1) FROM system_biz_menu), 6908));
